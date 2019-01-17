7665314863ce0f995eef77b834ac04a8774a84ca
Nuno Farinha <nadfarinha@gmail.com>
2019-01-17 14:14:13 +0000

    second test pass

--- friends.rb
+++ friends.rb
@@ -1,3 +1,7 @@
 def get_name(person)
   return person[:name]
 end
+
+def favourite_tv_show(person_2)
+  return person_2[:favourites][:tv_show]
+end
--- specs/friends_spec.rb
+++ specs/friends_spec.rb
@@ -73,6 +73,11 @@ class TestFriends < MiniTest::Test
 
   # 2. For a given person, return their favourite tv show
   # (e.g. the function favourite_tv_show(@person2) should return the string "Baywatch")
+def test_favourite_tv_show
+  result = favourite_tv_show(@person2)
+  assert_equal("Baywatch", result)
+end
+
 
 
   # 3. For a given person, check if they like a particular food
